0853549c00450bc098340e7b57e9e75abf1c45b8
Tawanda Andrew <xrthemc@gmail.com>
2021-11-03 10:18:23 +0200

    Update api.js
    Completing the /api/translate route

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,22 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const { Translator, TRANS_DIR } = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    const text = req.body.text;
+    const trans_dir = req.body.locale;
+    if (text === undefined || trans_dir === undefined)
+      return res.json({ error: "Required field(s) missing" });
+    if (text === "") return res.json({ error: "No text to translate" });
+    if (trans_dir !== TRANS_DIR.US2UK && trans_dir !== TRANS_DIR.UK2US)
+      return res.json({ error: "Invalid value for locale field" });
+
+    const translation = translator.translate(text, trans_dir);
+    if (translation === text)
+      return res.json({ text, translation: "Everything looks good to me!" });
+    return res.json({ text, translation });
+  });
 };
