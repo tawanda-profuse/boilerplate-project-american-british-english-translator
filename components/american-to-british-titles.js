c9a57737ca323467da229df082f32a5e7f635fa6
Tawanda Andrew <xrthemc@gmail.com>
2021-11-03 10:21:24 +0200

    Update american-to-british-titles.js
    I've decided to use capital letters instead of lowercase for the titles because I was failing to pass the freecodecamp tests

--- components/american-to-british-titles.js
+++ components/american-to-british-titles.js
@@ -1,8 +1,17 @@
-module.exports = {
+/* module.exports = {
   'mr.': 'mr',
   'mrs.': 'mrs',
   'ms.': 'ms',
   'mx.': 'mx',
   'dr.': 'dr',
   'prof.': 'prof'
-}
+} */
+
+module.exports = {
+  "Mr.": "Mr",
+  "Mrs.": "Mrs",
+  "Ms.": "Ms",
+  "Mx.": "Mx",
+  "Dr.": "Dr",
+  "Prof.": "Prof",
+};
